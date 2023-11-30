294298635bae7a9c58cfb81b24d3fe5d3ae59dbe
liyucheng <liyucheng_2@qq.com>
2023-11-30 14:30:32 +0000

    add gpt results parsing

--- correlations.py
+++ correlations.py
@@ -13,6 +13,25 @@ def load_model_predictions(path = '/mnt/fast/nobackup/users/yl02706/iconicity/mo
         scores[model_name] = df['score'].tolist()
     return scores
 
+def gpt_scores_and_correlation():
+    df = pd.read_csv('winter.csv')
+    gpt_35_df = pd.read_csv('gpt_3_5_iconicity_NEW.csv')
+    gpt_4_df = pd.read_csv('gpt_4_repl_iconicity.csv', names=['index', 'word', 'rating'])
+
+    gpt_35_scores = gpt_35_df['rating'].tolist()
+    gpt_35_gold = df.iloc[gpt_35_df['index'].tolist()]['rating'].tolist()
+
+    gpt_4_scores = gpt_4_df['rating'].tolist()
+    gpt_4_gold = df.iloc[gpt_4_df['index'].tolist()]['rating'].tolist()
+
+    print('GPT-3-5')
+    print('Spearman:', spearmanr(gpt_35_scores, gpt_35_gold))
+    print('Pearson:', pearsonr(gpt_35_scores, gpt_35_gold))
+    print('=' * 20)
+    print('GPT-4')
+    print('Spearman:', spearmanr(gpt_4_scores, gpt_4_gold))
+    print('Pearson:', pearsonr(gpt_4_scores, gpt_4_gold))
+
 if __name__ == '__main__':
     correlations = {'spearman': spearmanr, 'pearson': pearsonr}
 
